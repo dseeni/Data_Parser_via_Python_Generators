a996cbe3b51c0dc1cf64220cb9b4f53248c83494
Deepak Seeni <dseeni@gmail.com>
2019-07-06 01:57:41 -0700

    WIP car function

--- src/cars_generator.py
+++ src/cars_generator.py
@@ -1,8 +1,53 @@
-def file_reader(file):
-    with open(file) as file:
-        for line in file:
-            print(line)
+from collections import namedtuple
+from datetime import date
 
+filename = 'nyc_parking_tickets_extract.csv'
+#  Summons Number int
+#  Plate ID str
+#  Registration State str
+#  Plate Type str
+#  Issue Date date
+#  Violation Code int
+#  Vehicle Body Type str
+#  Vehicle Make str
+#  Violation Description str
+with open(filename) as file:
+    file_iter = iter(file)
+    headers = next(file_iter).strip('\n')
+    headers = headers.replace(" ","_")
+    print(headers)
+    print(type(headers))
+    data = next(file_iter).strip('\n').split(',')
+    # print(headers)
+    # print(type(headers))
+    # print(data)
+    Car = namedtuple('Car', headers)
+    headers = headers.split(',')
+    print(len(headers))
+    print(type(headers))
+    data_type = ['int', 'str', 'str', 'str', 'date', 'int', 'str', 'str', 'str']
+    cars = Car(*data)
+    cars_type = Car(*data_type)
+    print(cars)
+    print(cars_type)
+    
+    # print(len(cars))
+    # print(date_type)
+    # print(list(zip(data, date_type)))
 
-file_reader('nyc_parking_tickets_extract.csv')
+def cast(data_type, value):
+    if data_type == 'DOUBLE':
+        return float(value)
+    elif data_type == 'INT':
+        return int(value)
+    else:
+        return str(value)
 
+# def file_reader(self):
+#     with open(self.file) as csv:
+#         yield next(iter(csv))
+#         # for line in csv:
+#         #     print(line)
+
+# testdate = date(2012, 12, 12)
+# print(testdate)
