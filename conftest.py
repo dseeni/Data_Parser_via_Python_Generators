ed2b39a5c2687f20be5d3d65d6949dd656f3b3c2
Deepak Seeni <dseeni@gmail.com>
2019-07-10 00:15:06 -0700

    Add unit test for multiple frequency distribution counters

--- conftest.py
+++ conftest.py
@@ -3,6 +3,6 @@ from src.cars_generator import FileReader as Fr
 
 @fixture(scope='function')
 def test_file():
-    source_file = 'nyc_parking_tickets_extract.csv'
+    source_file = 'src/nyc_parking_tickets_extract.csv'
     file = Fr(source_file, 'Vehicle_Make', date_column=4)
     return file
--- tests/test_cars_generator.py
+++ tests/test_cars_generator.py
@@ -4,7 +4,7 @@ import types
 
 
 def test_file_reader_repr(test_file):
-    assert test_file.__repr__() == "FileReader(nyc_parking_tickets_extract.csv, Vehicle_Make, date_column=4)"
+    assert test_file.__repr__() == "FileReader(src/nyc_parking_tickets_extract.csv, Vehicle_Make, date_column=4)"
 
 
 def test_file_reader_is_iterable(test_file):
@@ -17,3 +17,19 @@ def test_file_reader_column_to_track_value_error():
     with raises(TypeError):
         file = Fr(source_file, 123, date_column=4)
         return file
+
+
+def test_file_reader_column_to_track_new_column(test_file):
+    test_file.column_to_track = 'Vehicle_Body_Type'
+    ml = []
+    for i in test_file:
+        ml.append(i)
+    assert test_file.highest_frequency_item == ('SUBN', 352)
+
+
+def test_file_reader_column_to_track_new_column_2(test_file):
+    test_file.column_to_track = 'Plate_Type'
+    ml = []
+    for i in test_file:
+        ml.append(i)
+    assert test_file.highest_frequency_item == ('PAS', 743)
