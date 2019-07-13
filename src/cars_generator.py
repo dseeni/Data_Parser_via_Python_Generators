1ab03e2a9581d95b96749ec4028f56c81c7017c3
Deepak Seeni <dseeni@gmail.com>
2019-07-12 21:44:24 -0700

    PEP 8 fixes and minor IDE warning fixes

--- src/cars_generator.py
+++ src/cars_generator.py
@@ -14,13 +14,13 @@ from datetime import date
 
 class FileReader:
 
-    def __init__(self, filename, column_to_track,*, date_column=None):
+    def __init__(self, filename, column_to_track, *, date_column: int = None):
         if not(isinstance(column_to_track, str)):
             raise TypeError('column_to_track must be of type str')
         self.filename = filename
         self.column_to_track = column_to_track
         self.column_counter = defaultdict(int)
-        self.date_column = date_column
+        self.date_column = int(date_column)
         self.data_key = None
         self.headers = None
         self.track_column_index_number = None
@@ -31,7 +31,7 @@ class FileReader:
     def __iter__(self):
         return FileReader.clean_row(self)
 
-    def sort_header_key(self):
+    def infer_data_type(self):
         for value in self.data_key:
             if value is None:
                 self.data_key[self.data_key.index(value)] = None
@@ -52,7 +52,7 @@ class FileReader:
             next(file)
             data_string = next(file).strip('\n')
             self.data_key = data_string.split(',')
-            self.sort_header_key()
+            self.infer_data_type()
         with open(self.filename) as file:
 
             # class io.TextTOWrapper
@@ -66,19 +66,20 @@ class FileReader:
             self.headers = headers_clean.split(',')
             self.track_column_index_number = self.headers.index(str(self.column_to_track))
             self.column_track_name = self.headers[self.track_column_index_number]
-            # namedtuple unpacking headers string seperting elements by comma
+            # namedtuple unpacking headers string separating elements by comma
             Row = namedtuple('Row', headers_clean)
-            headers_row = Row(*self.headers)
             yield self.headers
 
             for row in file:
                 # extract data as a list of strings from second row down
                 data: list = row.strip('\n').split(',')
-                final_data_list = \
-                    [FileReader.cast(data_type, value) for data_type, value in zip(self.data_key, data)]
+                final_data_list = ([FileReader.cast(data_type, value) for
+                                    data_type, value in zip(self.data_key, data)])
+                print('79:', 'final_data_list ''='' ', final_data_list)
 
                 # parse date into data object
                 if self.date_column is not None:
+                    # noinspection PyTypeChecker
                     final_data_list[self.date_column] = FileReader.date_modifier(final_data_list[self.date_column])
                 finaldatarow = Row(*final_data_list)
 
@@ -90,11 +91,13 @@ class FileReader:
                 else:
                     self.column_counter[getattr(finaldatarow, self.column_track_name)] = 1
 
-                self.column_counter_highest_frequency_key = \
-                    sorted(self.column_counter, key=lambda k: self.column_counter[k], reverse=True)
+                # return item name
+                self.column_counter_highest_frequency_key = sorted(self.column_counter, key=lambda k:
+                                                                   self.column_counter[k], reverse=True)
+                
+                # return tuple(item, frequency)
                 self.highest_frequency_item = (self.column_counter_highest_frequency_key[0],
-                                               self.column_counter.get
-                                               (self.column_counter_highest_frequency_key[0]))
+                                               self.column_counter.get(self.column_counter_highest_frequency_key[0]))
                 yield finaldatarow
         yield ('File Processed!')
 
@@ -112,9 +115,11 @@ class FileReader:
             return str(data_value)
 
     @staticmethod
-    def date_modifier(date_string):
+    def date_modifier(date_string: str) -> date:
         date_list = date_string.split('/')
+        # noinspection PyUnusedLocal
         date_format_key = ['int' for i in range(3)]
+        print('121:', 'date_format_key ''='' ', date_format_key)
         finaldate = [FileReader.cast(date_format_key, date_list)
                      for date_format_key, date_list in zip(date_format_key, date_list)]
         assert all(isinstance(i, int) for i in finaldate)
@@ -128,9 +133,10 @@ class FileReader:
         return 'FileReader({0}, {1}, date_column={2})'.format(self.filename, self.column_to_track, self.date_column)
 
 
-# file = FileReader(source_file, 'Vehicle_Body_Type', date_column=4)
-# filegen = iter(file)
-# for i in filegen:
-#     print(i)
+# source_file = 'nyc_parking_tickets_extract.csv'
+# file_reader = FileReader(source_file, 'Vehicle_Make', date_column=4)
+# filegen = iter(file_reader)
+# for line in filegen:
+#     print(line)
 #
-# print('136:', 'file.highest_frequency_item ''='' ', file.highest_frequency_item)
+# print('136:', 'file.highest_frequency_item ''='' ', file_reader.highest_frequency_item)
